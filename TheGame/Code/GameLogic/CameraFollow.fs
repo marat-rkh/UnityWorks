6c10623abe6b37ae3c4510e6d95881a933265f02
octomarat <maratx387@gmail.com>
2016-02-07 00:26:32 +0300

    New sprites and cammera follow created

---
+++ TheGame/Code/GameLogic/CameraFollow.fs
@@ -0,0 +1,22 @@
+﻿namespace Main
+
+open UnityEngine
+
+type CameraFollow() = 
+    inherit MonoBehaviour()
+
+    let mutable gregController = null
+    let mutable velocity = Vector2()
+
+    [<SerializeField>]
+    let mutable smoothTimeX = 0.25f
+    [<SerializeField>]
+    let mutable smoothTimeY = 0.25f
+
+    member x.Start () = 
+        gregController <- GameObject.FindGameObjectWithTag("Player")
+
+    member x.FixedUpdate () =
+        let posX = Mathf.SmoothDamp(x.transform.position.x, gregController.transform.position.x, &velocity.x, smoothTimeX)
+        let posY = Mathf.SmoothDamp(x.transform.position.y, gregController.transform.position.y, &velocity.y, smoothTimeY)
+        x.transform.position = Vector3(posX, posY, x.transform.position.z)
